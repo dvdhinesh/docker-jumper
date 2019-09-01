6096d1439ef3b92b340d17c0c1d711af84eef454
Dhinesh D <dvdhinesh.mail@gmail.com>
2019-09-01 13:59:01 +0800

    Added docker-jumper

---
+++ php-on-apache/serving-app/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Example Title</title>
+    </head>
+    <body>
+        <p>
+            Hello. MyIP is <?= $_SERVER['SERVER_ADDR'] ?>
+        </p>
+    </body>
+</html>
